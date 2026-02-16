bb7776ddbb065177cb5ce663522f906872c6aedd
meenu10802 <meenu10802>
2026-02-16 12:54:10 +0530

    [Meenakshi]: Added UC1 - Display puzzle title

---
+++ src/LexicalTwistPuzzle.java
@@ -0,0 +1,20 @@
+/**
+ * LexicalTwistPuzzle
+ *
+ * This class represents the entry point of the Lexical Twist Puzzle application.
+ * It currently only displays the application title.
+ *
+ * Concepts Used:
+ * - Class declaration
+ * - main() method
+ * - Console output
+ */
+public class LexicalTwistPuzzle {
+
+    public static void main(String[] args) {
+
+        // Display the application title
+        System.out.println("Lexical Twist Puzzle");
+
+    }
+}
